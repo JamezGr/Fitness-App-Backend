f2f653ace69534d73f73fddadde548755ae6d53f
JamezGr <james.p.grimes1999@gmail.com>
2021-09-19 17:10:03 +0100

    Second Attempt To Fix CORS.

--- api/endpoints/default.py
+++ api/endpoints/default.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 from flask import jsonify
+from flask_cors import cross_origin
 
 from api.response import ErrorMessage
 
 blueprint = Blueprint(name="default_endpoint", import_name=__name__)
 
+@cross_origin
 @blueprint.route('/', methods=['GET', 'POST'])
 def default_response():
     return jsonify(ErrorMessage.INVALID_REQUEST), 401
